53ddf0b5f413d54cd049948b57408b12eab3c8a3
Fraven <65469177+FravenSancho@users.noreply.github.com>
2022-07-04 19:51:31 -0500

    Create e_10.py

---
+++ e_10.py
@@ -0,0 +1,9 @@
+def area_triangulo(base, altura):
+  print(base * altura /2)
+
+def area_circulo(radio):
+  pi = 3.14
+  print(pi * (radio**2))
+
+prueba1 = area_triangulo(8,4)
+prueba2 = area_circulo(9)
