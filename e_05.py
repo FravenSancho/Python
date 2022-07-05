d8b63609c9bb32eb02e7c37e84c4b99872005326
Fraven <65469177+FravenSancho@users.noreply.github.com>
2022-07-04 19:02:10 -0500

    Create e_05.py

---
+++ e_05.py
@@ -0,0 +1,20 @@
+Nombre = "Fraven"
+Apellidos = "Sancho Lazo"
+Edad = 26
+Email = 'fraven.slazo@gmail.com'
+Telefono = 977592124
+Casado = False
+Con_Hijos = False
+list_amigos = ['Chedania', 'Roberto', 'Camila', 'Eduardo']
+Peliculas = {
+    'título' : ['Piratas del Caribe', 'Star Wars', 'Doctor Strange']
+}
+print(Nombre)
+print(Apellidos)
+print(Edad)
+print(Email)
+print(Telefono)
+print(Casado)
+print(Con_Hijos)
+print(list_amigos)
+print(Peliculas)
