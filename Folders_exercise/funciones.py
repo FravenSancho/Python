ebb74f10383cb803a87083f545328542a3942137
Fraven <65469177+FravenSancho@users.noreply.github.com>
2022-07-12 00:21:29 -0500

    Add files via upload

---
+++ Folders_exercise/funciones.py
@@ -0,0 +1,19 @@
+class Operaciones():
+
+    def __init__(self,num1,num2):
+        self.num1 = num1
+        self.num2 = num2
+
+    def sumar(self):
+        return self.num1 + self.num2
+
+    def restar(self):
+        return self.num1 - self.num2
+    def multi(self):
+        return self.num1 * self.num2
+
+    def dividir(self):
+        return self.num1 / self.num2
+
+
+    
